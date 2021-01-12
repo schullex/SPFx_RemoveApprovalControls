bdf230c34d950a1f62406173ef1e9b1b372f48f3
Stefan Schulte <stefan.schulte@cosmer.com>
2021-01-12 09:50:16 +0100

    excludeHashFromFileNames

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 const build = require('@microsoft/sp-build-web');
 
+build.copyAssets.taskConfig = { excludeHashFromFileNames: true, }
+
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
 build.initialize(require('gulp'));
